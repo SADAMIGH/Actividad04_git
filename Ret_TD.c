9215b4570c6bf196e934de8437e647cd0c644641
Sadam Irak <sadam.gonzalez5677@alumnos.udg.mx>
2020-09-20 18:33:25 -0500

    Parte 2 capturar enteros

--- Ret_TD.c
+++ Ret_TD.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void capturar_enteros (){
+
+int n, datos[5];
+float sum_prom=0;
+
+    for(n=0; n<=4; n++)
+    {
+        printf("Introduce el numero %d: ", n+1);
+        scanf("%d", &datos[n]);
+    } 
+    printf("\nLos numeros introducidos son: ");
+    for(n=0; n<=4; n++)
+    {
+        printf("%d, ",datos[n]);
+        sum_prom=sum_prom+datos[n];
+    }
+    printf("\nEl resultado de sumar los numeros introducidos es: %.0f\n",sum_prom);
+    sum_prom=sum_prom/(n);
+    printf("El promedio de los numeros introducidos es: %.2f\n\n",sum_prom);
+
+}
 
 int main ()
 {
@@ -12,8 +33,24 @@ int main ()
         printf("3) Agregar personaje\n");
         printf("0) Salir\n");
         scanf("%c", &opc);
+
+        switch (opc)
+        {
+            case '1':
+            capturar_enteros();
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            default:
+                break;       
+        }
+
         fflush(stdin);
     }while(opc != '0');
+    
+
     
     return 0;
 }
